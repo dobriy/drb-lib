b0784e871ff56232c6870dc26fefdf375573c294
Alexey Popov <dobriy1@gmail.com>
2012-02-15 15:34:04 +0000

    +WHITE_SPACE symbol

--- src/drb/Constants/Symbols.java
+++ src/drb/Constants/Symbols.java
@@ -8,8 +8,8 @@ package drb.Constants;
  *
  * @author alexey
  */
-public class Symbols
-{
+public class Symbols {
 
-   public static final String EMPTY_STRING = "";
+    public static final String EMPTY_STRING = "";
+    public static final String WHITE_SPACE = " ";
 }
